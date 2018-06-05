747a56bada6b936e9edc5b0ccba18eed4c4e2aec
Jessica <jmc617@gmail.com>
2018-06-05 03:34:52 -0400

    blog creation functiional

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,27 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    @blog.user_id = current_user.id
+    if @blog.save
+      # flash[:message] = 'blog created'
+      redirect_to "/"
+    else
+      # flash[:message] = 'please try again'
+      render "/blogs/new"
+    end
+  end
+
+  def edit
+  end
+end
+
+def blog_params
+  params.require(:blog).permit(:title, :content)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'blogs/index' => "blogs#index"
+  # get 'blogs/new' => "blogs#new"
+  # get 'blogs/edit' => "blogs#edit"
+  get 'welcome/index' => "welcome#index"
   devise_for :users
   root 'welcome#index'
+  resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
