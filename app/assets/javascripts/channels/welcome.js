5855ab48bbbc62ca27f004ed27a4fc04a53ad94e
Jessica <jmc617@gmail.com>
2018-06-06 14:42:18 -0400

    wjatever

--- app/assets/javascripts/channels/welcome.js
+++ app/assets/javascripts/channels/welcome.js
@@ -1,8 +1,8 @@
 // 1.wrap functions to have them work when page loaded
-window.addEventListenter("DOMContentLoaded", function(){
-
-})
-// OR 2. add jquery gem in rails Gemfile/bundle install and require in application.js
-$(document).ready(function({
-
-}))
+// window.addEventListenter("DOMContentLoaded", function(){
+//
+// })
+// // OR 2. add jquery gem in rails Gemfile/bundle install and require in application.js
+// $(document).ready(function({
+//
+// }))
