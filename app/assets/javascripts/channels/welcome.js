3167fa77fc09b8281a007e0e783063defe88d24f
Jessica <jmc617@gmail.com>
2018-06-10 23:10:20 -0400

    JS functional and styling changes

--- app/assets/javascripts/channels/welcome.js
+++
@@ -1,8 +0,0 @@
-// // 1.wrap functions to have them work when page loaded
-// window.addEventListenter("DOMContentLoaded", function(){
-//
-// })
-// // OR 2. add jquery gem in rails Gemfile/bundle install and require in application.js
-// $(document).ready(function({
-//
-// }))
---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,21 @@
+// // 1.wrap functions to have them work when page loaded
+window.addEventListener("DOMContentLoaded", function(){
+  var commentBtns = document.getElementsByClassName('comment-btns')
+  var commentForms = document.getElementsByClassName('comment-forms')
+
+
+for (let i = 0; i < commentBtns.length; i++) {
+  commentBtns[i].addEventListener('click', function(){
+    commentForms[i].style.display = 'block'
+    commentBtns[i].style.display = 'none'
+  })
+}
+})
+// // 1.wrap functions to have them work when page loaded
+// window.addEventListenter("DOMContentLoaded", function(){
+//
+// })
+// // OR 2. add jquery gem in rails Gemfile/bundle install and require in application.js
+// $(document).ready(function({
+//
+// }))
